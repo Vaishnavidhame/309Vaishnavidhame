e438d41163a68d6a53369b34cbe104df7404e74f
Vaishnavidhame <68319887+Vaishnavidhame@users.noreply.github.com>
2020-12-01 09:48:19 +0530

    Update Assignment 3.py

--- Assignment 3.py
+++ Assignment 3.py
@@ -4,8 +4,19 @@ import pandas as pd
 from google.colab import files
 uploaded=files.upload()
 
-
+dataset=pd.read_csv("indian_food.csv")
 dataset 
 
-dataset.plot.line()
+dataset.plot.bar()
+plt.title("bar plot")
+Text(0.5, 1.0, 'bar plot')
+
+dataset.plot.hist()
+plt.title("histogram graph")
+Text(0.5, 1.0, 'histogram graph')
+
+dataset. plot.line()
 plt.title("line garph")
+Text(0.5, 1.0, 'line garph')
+
+
